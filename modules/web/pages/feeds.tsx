0646d949818a989d801dc3711e4fab4b68494ba9
Jay Feldman <jay.feldman03@gmail.com>
2022-09-22 20:52:58 -0400

    create sidebar

--- modules/web/pages/feeds.tsx
+++ modules/web/pages/feeds.tsx
@@ -40,51 +40,61 @@ const FeedsPage = () => {
   return (
     <div>
       <PageHead unreadCount={unreadCount} />
-
       <Background>
-        <h1>Welcome to Simple RSS</h1>
-        {errorMessage ? <h5 className="text-danger">{errorMessage}</h5> : null}
-        {isFetching ? <Spinner /> : null}
-        {hasFetched && !items?.length ? (
-          <h5 className="py-5">All read!</h5>
-        ) : null}
+        {/* Sidebar */}
+        <div className="d-flex flex-row">
+          <div className="d-flex flex-column align-items-start col-2">
+            {showFetchAll ? (
+              <Button className="my-2" onClick={() => setFetchAll(true)}>
+                Get all items
+              </Button>
+            ) : null}
+            <div />
+            {showMarkAllRead ? (
+              <Button className="my-2" onClick={markAllRead}>
+                Mark all as read
+              </Button>
+            ) : null}
+            <div />
+            <SubmitButton className="my-2" onClick={onLogOut}>
+              Log out
+            </SubmitButton>
+            <div />
 
-        <Row xs={1} md={2} lg={3} xl={4} className="g-4 text-dark">
-          {items?.map(item => (
-            <FeedCard key={item.id} item={item} onItemClick={onItemClick} />
-          ))}
-        </Row>
-        {showFetchAll ? (
-          <Button className="col-sm-2 my-3" onClick={() => setFetchAll(true)}>
-            Get all items
-          </Button>
-        ) : null}
-        <div />
-        {showMarkAllRead ? (
-          <Button className="col-sm-2 my-3" onClick={markAllRead}>
-            Mark all as read
-          </Button>
-        ) : null}
-        <div />
-        <SubmitButton className="my-2" onClick={onLogOut}>
-          Log out
-        </SubmitButton>
-        <div />
+            <Form.Label className="my-2">Add Feed</Form.Label>
+            <Form.Control
+              onKeyDown={e =>
+                e.key === 'Enter' && addFeed(e.currentTarget.value)
+              }
+            />
 
-        <Form.Label className="my-2">Add Feed</Form.Label>
-        <Form.Control
-          onKeyDown={e => e.key === 'Enter' && addFeed(e.currentTarget.value)}
-        />
-
-        <SubmitButton
-          className="my-4"
-          onClick={() => deleteFeed(items?.[0]?.feedId ?? '')}>
-          Delete feed for first item
-        </SubmitButton>
-        <div />
-        <SubmitButton className="my-4" onClick={deleteUser}>
-          Delete user
-        </SubmitButton>
+            <SubmitButton
+              className="my-4"
+              onClick={() => deleteFeed(items?.[0]?.feedId ?? '')}>
+              Delete feed for first item
+            </SubmitButton>
+            <div />
+            <SubmitButton className="" onClick={deleteUser}>
+              Delete user
+            </SubmitButton>
+          </div>
+          {/* Main body */}
+          <div className="d-flex-inline col px-5">
+            <h1>Welcome to Simple RSS</h1>
+            {errorMessage ? (
+              <h5 className="text-danger">{errorMessage}</h5>
+            ) : null}
+            {isFetching ? <Spinner /> : null}
+            {hasFetched && !items?.length ? (
+              <h5 className="py-5">All read!</h5>
+            ) : null}
+            <Row xs={1} md={2} lg={3} xl={4} className="g-4 text-dark">
+              {items?.map(item => (
+                <FeedCard key={item.id} item={item} onItemClick={onItemClick} />
+              ))}
+            </Row>
+          </div>
+        </div>
       </Background>
     </div>
   );
