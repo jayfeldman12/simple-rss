bab8f81a34da6e4eaa6716ed68fd1d43951d0ab2
Jay Feldman <jay.feldman03@gmail.com>
2022-10-16 16:47:16 -0400

    reset to 20 minutes

--- modules/web/utils/consts.ts
+++ modules/web/utils/consts.ts
@@ -1,2 +1,2 @@
-export const APP_FEED_REFRESH_TIME = 1000 * 60 * 8; // 8 minutes
+export const APP_FEED_REFRESH_TIME = 1000 * 60 * 20; // 20 minutes
 export const USER_REFRESH_TIME = 1000 * 60 * 60 * 24; // 1 day, users won't refresh often
